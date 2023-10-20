4f3f7adac49c178530826e45e726804d151c74e4
Igor Augusto <igoraugustobz@gmail.com>
2023-10-20 18:00:25 -0300

    feat: adição do projeto

---
+++ todolist-api/src/test/java/vercel/iaugusto/todolistapi/TodolistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vercel.iaugusto.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

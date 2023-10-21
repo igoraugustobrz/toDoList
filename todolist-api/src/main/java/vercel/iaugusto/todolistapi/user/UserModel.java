8f045be14877ad7c6e117e34a42463d501bdde0b
Igor Augusto <igoraugustobz@gmail.com>
2023-10-21 17:36:41 -0300

    chore: adição de novas dependências

--- todolist-api/src/main/java/vercel/iaugusto/todolistapi/user/UserModel.java
+++ todolist-api/src/main/java/vercel/iaugusto/todolistapi/user/UserModel.java
@@ -1,8 +1,26 @@
 package vercel.iaugusto.todolistapi.user;
 
-public class UserModel {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_users")
+public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }

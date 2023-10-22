cc7d3567f45516ba0594a0d935f2132f720713c3
Igor Augusto <igoraugustobz@gmail.com>
2023-10-22 11:15:07 -0300

    feat: adicionada a funcionalidade de cadastro de tarefas

---
+++ todolist-api/src/main/java/vercel/iaugusto/todolistapi/task/TaskModel.java
@@ -0,0 +1,26 @@
+package vercel.iaugusto.todolistapi.task;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    private UUID idUser;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
